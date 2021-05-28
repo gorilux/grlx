920021ed1e912db3955a1bd69daa6ca67ee89c59
Osvaldo Punzetti <osvaldo.punzetti@share2cloud.com>
2021-05-28 09:59:56 +0200

    Changed CMakeLists to support emcscripten

--- cmake/GrlxDependencies.cmake
+++ cmake/GrlxDependencies.cmake
@@ -1,5 +1,8 @@
 
+
+if (NOT ${CMAKE_SYSTEM_NAME} MATCHES "Emscripten")
 include(${CMAKE_CURRENT_LIST_DIR}/dependencies/openssl.cmake)
+endif()
 include(${CMAKE_CURRENT_LIST_DIR}/dependencies/boost.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/dependencies/json.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/dependencies/url.cmake)
--- cmake/dependencies/boost.cmake
+++ cmake/dependencies/boost.cmake
@@ -1,4 +1,20 @@
+
 set(BOOST_VERSION 1.72.0)
+if (NOT ${CMAKE_SYSTEM_NAME} MATCHES "Emscripten")
+set(BOOST_COMPONENTS
+    context
+    coroutine
+    date_time
+    filesystem
+    iostreams
+    program_options
+    regex
+    system
+    unit_test_framework
+)
+find_package(Boost ${BOOST_VERSION} REQUIRED COMPONENTS ${BOOST_COMPONENTS})
+find_package(Threads REQUIRED)
+elseif()
 set(BOOST_COMPONENTS
     context
     coroutine
@@ -11,8 +27,8 @@ set(BOOST_COMPONENTS
     unit_test_framework
 )
 
+set_target_properties(Boost PROPERTIES COMPILE_FLAGS "-s USE_BOOST_HEADERS=1 -s ERROR_ON_UNDEFINED_SYMBOLS=0" LINK_FLAGS "-s USE_BOOST_HEADERS=1 -s ERROR_ON_UNDEFINED_SYMBOLS=0")
+endif()
 
 
 
-find_package(Boost ${BOOST_VERSION} REQUIRED COMPONENTS ${BOOST_COMPONENTS})
-find_package(Threads REQUIRED)
